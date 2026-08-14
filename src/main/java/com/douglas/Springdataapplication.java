cefecd8e2c0adbe323ff64e2db57285e59982ad9
douglasinforj <douglasitpro@gmail.com>
2026-08-14 17:18:57 -0300

    chore(pom): Adicionado dependencias do projeto
    feat(app): adicionado estrutura Springdataapplication para inicialização

---
+++ src/main/java/com/douglas/Springdataapplication.java
@@ -0,0 +1,12 @@
+package com.douglas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springdataapplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Springdataapplication.class, args);
+    }
+}
